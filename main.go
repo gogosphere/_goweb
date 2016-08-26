265d979738b1838021cbdc4d65debc5a3b7f6722
Bill Hancock <whancock@homeaway.com>
2016-08-26 14:25:49 -0500

    update main

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/describe-instances", JsonFull)
-	router.HandleFunc("/{calledfile}", CalledFile)
+	router.HandleFunc("/{calledfile}.{ext:html}", CalledFileHTML)
+	router.HandleFunc("/{calledfile}.{ext:json}", CalledFileJson)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -35,23 +35,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(file))
 }
 
-func JsonFull(w http.ResponseWriter, r *http.Request) {
-
-	file, e := ioutil.ReadFile("describe-instances.json")
-	if e != nil {
-		fmt.Fprintln(w, e, "ioutil read bork")
-	}
-	/*
-		_, err := json.Marshal(file)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	*/
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	fmt.Fprintln(w, string(file))
-}
-
 func CalledFile(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	calledfile := vars["calledfile"]
@@ -59,13 +42,33 @@ func CalledFile(w http.ResponseWriter, r *http.Request) {
 	if e != nil {
 		fmt.Fprintln(w, e, "ioutil read bork")
 	}
-	/*
-		_, err := json.Marshal(file)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	*/
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	fmt.Fprintln(w, string(file))
+}
+
+func CalledFileHTML(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	calledfile := vars["calledfile"]
+	fileext := vars["ext"]
+	file, e := ioutil.ReadFile("data/" + calledfile + "." + fileext)
+	if e != nil {
+		fmt.Fprintln(w, e, "ioutil read bork")
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	fmt.Fprintln(w, string(file))
+}
+
+func CalledFileJson(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	calledfile := vars["calledfile"]
+	fileext := vars["ext"]
+	file, e := ioutil.ReadFile("data/" + calledfile + "." + fileext)
+	if e != nil {
+		fmt.Fprintln(w, e, "ioutil read bork")
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	fmt.Fprintln(w, string(file))
 }
